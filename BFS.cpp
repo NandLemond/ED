5d46751d043a6ef9aaf7f7572a164cb43c51aa72
NandLemond <fernandosuca8@gmail.com>
2023-12-31 11:13:08 -0500

    implementa la idea de crear un un vector que contenga los hijos inmediatos de del primer valor ingresado en la queue, esto lo hice con el proposito de poder iterar sobre los hijos de ese nodo seleccionado de la cola

--- BFS.cpp
+++ BFS.cpp
@@ -4,9 +4,11 @@
 #include <vector>
 
 using namespace std;
-
+Arbol* nodePadre;
 int n_nodos = 0;
-
+char caracter_padre = ' ';
+vector<char>hijos;
+Arbol* padre_= nullptr;
 void  nodosInternos(Arbol *raiz){
   if (raiz == NULL) {
     return;
@@ -38,17 +40,76 @@ void nodos_totales(Arbol* raiz){
   nodosHoja(raiz);
   nodosInternos(raiz);
 }
-void BFS(Arbol* Tree,char value){
-   queue<char> fila;
-   vector<char> mark;
-   mark.push_back(Tree->valor);
-   fila.push(Tree->valor);
-  // cout<<fila.front()<<mark.at(0)<<endl;
-   
+void padre_de(Arbol* raiz, char letra, Arbol* padre ) {
+  if (raiz == NULL) {
+	return;
+  }
 
+  if (raiz->valor == letra) {
+      //nodoBuscado = raiz;
+       padre_ = padre;
+      return ; 
+  }
 
-
+  padre_de (raiz->izqu,letra,raiz);
+  padre_de (raiz->med, letra,raiz);
+  padre_de (raiz->dere, letra,raiz);
 }
+
+void node_inme(Arbol* raiz,Arbol* node,char x){
+  char aux = ' ';
+  if(node == NULL){
+      return;
+  }
+ padre_de(raiz,node->valor,NULL); 
+ 
+ if(padre_ == NULL){
+    aux = ' ';
+ }
+ else {
+    aux = padre_->valor;
+ }
+
+  if(  aux == x && padre_!= NULL){
+      hijos.push_back(node->valor);
+
+      return;
+  }
+ 
+  node_inme(raiz,node->izqu,x);
+  node_inme(raiz,node->med,x);
+  node_inme(raiz, node->dere,x);
+}
+void imprimirVector(const vector<char>& vec) {
+    cout << "Elementos del vector: ";
+    for (const auto& elemento : vec) {
+        std::cout << elemento << " ";
+    }
+    cout << endl;
+}
+void BFS(Arbol* Tree) {
+    if (Tree == nullptr) {
+        cout << "El árbol es nulo." << endl;
+        return;
+    }
+
+    queue<Arbol*> fila;
+    vector<Arbol*> mark;
+    mark.push_back(Tree);
+    fila.push(Tree);
+
+    while (!fila.empty()) {
+        Arbol* node_x = fila.front();
+        fila.pop();
+
+        if (node_x != nullptr) {
+            caracter_padre = node_x->valor;
+            node_inme(Tree, node_x, caracter_padre);
+            imprimirVector(hijos);
+        }
+    }
+}
+
 int main(){
     Arbol* arbolTernario = new Arbol('F');
 
@@ -79,5 +140,5 @@ int main(){
 
     arbolTernario->dere->izqu->izqu->izqu = new Arbol('P');
     nodos_totales(arbolTernario); 
-    BFS(arbolTernario,'a');
+    BFS(arbolTernario);
 }
